ed3d97d646b83ffe077f4ac837cf389636fd0b84
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-14 16:18:43 -0700

    added jwt but wont save on refresh

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -15,30 +15,26 @@ function LoginForm() {
 
     const [errorMessage, setErrorMessage] = useState(null)
 
-      
-async function handleSubmit(e) {
-    e.preventDefault()
-    const response = await fetch(`http://localhost:5001/authentication/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const response = await fetch(`http://localhost:5001/authentication/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
 
-    const data = await response.json()
+        const data = await response.json()
 
-    if (response.status === 200) {
-        setCurrentUser(data.user)
-        history.push(`/`)
-    } else {
-        setErrorMessage(data.message)
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
+            history.push(`/`)
+        } else {
+            setErrorMessage(data.message)
+        }
     }
-
-    console.log(data)
-}
-  
-
 
     return (
         <main>
