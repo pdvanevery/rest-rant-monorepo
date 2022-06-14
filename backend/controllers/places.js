21d53b982645836d38f0d1122280acfa288ed040
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-13 19:42:09 -0700

    new commit

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -94,16 +94,26 @@ router.post('/:placeId/comments', async (req, res) => {
         res.status(404).json({ message: `Could not find place with id "${placeId}"` })
     }
 
-    const author = await User.findOne({
-        where: { userId: req.body.authorId }
-    })
+    let currentUser; 
+    try {
+        currentUser = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+    } catch {
+        currentUser = null
+    }
 
-    if (!author) {
-        res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
+    if (!currentUser) {
+        return res.status(404).json({
+            message: `You must be logged in to leave a rant or rave.`
+        })
     }
 
     const comment = await Comment.create({
         ...req.body,
+        autherId: currentUser.userId,
         placeId: placeId
     })
 
--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -54,10 +54,10 @@ function NewCommentForm({ place, onSubmit }) {
             </div>
             <div className="row">
                 <div className="form-group col-sm-4">
-                    <label htmlFor="state">Author</label>
+                    {/* <label htmlFor="state">Author</label>
                     <select className="form-control" value={comment.authorId} onChange={e => setComment({ ...comment, authorId: e.target.value })}>
                         {authorOptions}
-                    </select>
+                    </select> */}
                 </div>
                 <div className="form-group col-sm-4">
                     <label htmlFor="stars">Star Rating</label>
